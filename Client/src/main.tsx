bd9f8b333f56c9d3fb3ef6d69e8a24e4f0351e28
Karol-Wisniewski <karol-wisniewski2001@wp.pl>
2023-10-21 17:21:49 +0200

    removed unnecessary Theme type declaration

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { ThemeProvider, Theme } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
 import theme from './Style/themes/themes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme as Theme}>
+  <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>
 )
