3f6353a3a049463d864bb879ac0148acc1883575
Karol-Wisniewski <karol-wisniewski2001@wp.pl>
2023-10-21 17:15:26 +0200

    changed themes.ts import extension

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,8 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider, Theme } from '@mui/material/styles'
-//@ts-ignore
-import theme from './Style/themes/themes.ts'
+import theme from './Style/themes/themes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={theme as Theme}>
