ae9f5e21eb8865fd94f58e6bb3fe979f5303cf22
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2023-10-21 14:50:05 +0200

    added eureka client

--- UserService/src/main/java/it/homeo/userservice/UserServiceApplication.java
+++ UserService/src/main/java/it/homeo/userservice/UserServiceApplication.java
@@ -2,11 +2,13 @@ package it.homeo.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
