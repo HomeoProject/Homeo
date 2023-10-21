3ebdc6809146693fa07b43004196ce8f4efa1766
Karol-Wisniewski <karol-wisniewski2001@wp.pl>
2023-10-21 17:12:10 +0200

    changed linter config for dev purposes

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider, Theme } from '@mui/material/styles'
+//@ts-ignore
 import theme from './Style/themes/themes.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
