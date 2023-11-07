55e3f00c9fd82901dcd4933efd1883b72aac5810
Maciej Słupianek <maciej.slupianekge@gmail.com>
2023-11-07 21:05:25 +0100

    prettier-upd

--- Client/src/Components/UserCard.tsx
+++ Client/src/Components/UserCard.tsx
@@ -10,88 +10,90 @@ import StarOutlineIcon from '@mui/icons-material/StarOutline'
 import SmartphoneIcon from '@mui/icons-material/Smartphone'
 import '../style/scss/components/UserCard.scss'
 
-const UserCard = ({isDialog}) => {
-  const [open, setOpen] = useState(false)
+const UserCard = ({ isDialog }) => {
+    const [open, setOpen] = useState(false)
 
-  const handleClickOpen = () => {
-    setOpen(true)
-  }
+    const handleClickOpen = () => {
+        setOpen(true)
+    }
 
-  const handleClose = () => {
-    setOpen(false)
-  }
+    const handleClose = () => {
+        setOpen(false)
+    }
 
-  // some
+    // some
 
-  return (
-    <div className="UserCard">
-      <div className="user-card-avatar">
-        <img src={Karol} alt="avatar" />
-      </div>
-      <Card sx={{ padding: '15px' }}>
-        <div className="user-card-container">
-          <div className="user-card-container-user-section">
-            <span className="user-card-container-user-section-name">Darek</span>
-            <span className="user-card-container-user-section-title">
-              Plumber
-            </span>
-          </div>
-          <div className="user-card-container-info-section">
-            <div className="user-card-container-info-section-row">
-              <SmartphoneIcon />
-              <span className="user-card-container-info-section-row-value">
-                501 474 375
-              </span>
+    return (
+        <div className="UserCard">
+            <div className="user-card-avatar">
+                <img src={Karol} alt="avatar" />
             </div>
-            <div className="user-card-container-info-section-row">
-              <PublicIcon />
-              <span className="user-card-container-info-section-row-value">
-                Pomorskie, Poland
-              </span>
-            </div>
-            <div className="user-card-container-info-section-row">
-              <MailOutlineIcon />
-              <span className="user-card-container-info-section-row-value">
-                darek@gmail.com
-              </span>
-            </div>
-            <div className="user-card-container-info-section-row">
-              <AttachMoneyIcon />
-              <span className="user-card-container-info-section-row-value">
-                40-50 zł/h
-              </span>
-            </div>
-            <div className="user-card-container-info-section-row">
-              <StarOutlineIcon />
-              <span className="user-card-container-info-section-row-value">
-                4.5
-              </span>
-            </div>
-          </div>
-          <div className="user-card-container-footer">
-            {isDialog ? (
-              <Button
-              variant="contained"
-              sx={{ width: '100%' , fontWeight: 700}}
-              onClick={handleClickOpen}
-              >
-              Go to profile
-            </Button>
-            ) : (
-              <Button
-              variant="contained"
-              sx={{ width: '100%' , fontWeight: 700}}
-              onClick={handleClickOpen}
-              >
-              More
-            </Button>
-            )}
-          </div>
+            <Card sx={{ padding: '15px' }}>
+                <div className="user-card-container">
+                    <div className="user-card-container-user-section">
+                        <span className="user-card-container-user-section-name">
+                            Darek
+                        </span>
+                        <span className="user-card-container-user-section-title">
+                            Plumber
+                        </span>
+                    </div>
+                    <div className="user-card-container-info-section">
+                        <div className="user-card-container-info-section-row">
+                            <SmartphoneIcon />
+                            <span className="user-card-container-info-section-row-value">
+                                501 474 375
+                            </span>
+                        </div>
+                        <div className="user-card-container-info-section-row">
+                            <PublicIcon />
+                            <span className="user-card-container-info-section-row-value">
+                                Pomorskie, Poland
+                            </span>
+                        </div>
+                        <div className="user-card-container-info-section-row">
+                            <MailOutlineIcon />
+                            <span className="user-card-container-info-section-row-value">
+                                darek@gmail.com
+                            </span>
+                        </div>
+                        <div className="user-card-container-info-section-row">
+                            <AttachMoneyIcon />
+                            <span className="user-card-container-info-section-row-value">
+                                40-50 zł/h
+                            </span>
+                        </div>
+                        <div className="user-card-container-info-section-row">
+                            <StarOutlineIcon />
+                            <span className="user-card-container-info-section-row-value">
+                                4.5
+                            </span>
+                        </div>
+                    </div>
+                    <div className="user-card-container-footer">
+                        {isDialog ? (
+                            <Button
+                                variant="contained"
+                                sx={{ width: '100%', fontWeight: 700 }}
+                                onClick={handleClickOpen}
+                            >
+                                Go to profile
+                            </Button>
+                        ) : (
+                            <Button
+                                variant="contained"
+                                sx={{ width: '100%', fontWeight: 700 }}
+                                onClick={handleClickOpen}
+                            >
+                                More
+                            </Button>
+                        )}
+                    </div>
+                </div>
+            </Card>
+            <UserCardDialog open={open} handleClose={handleClose} />
         </div>
-      </Card>
-      <UserCardDialog open={open} handleClose={handleClose} />
-    </div>
-  )
+    )
 }
 
 export default UserCard
--- Client/src/Components/UserCardDialog.tsx
+++ Client/src/Components/UserCardDialog.tsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from 'react'
+import { useRef, useState } from 'react'
 
 import UserCard from './UserCard'
 import '../style/scss/components/UserCardDialog.scss'
@@ -11,141 +11,171 @@ import AccordionDetails from '@mui/material/AccordionDetails'
 import Button from '@mui/material/Button'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import TextField from '@mui/material/TextField'
-import SendIcon from '@mui/icons-material/Send';
+import SendIcon from '@mui/icons-material/Send'
 
 export interface SimpleDialogProps {
-  open: boolean
-  handleClose: () => void
+    open: boolean
+    handleClose: () => void
 }
 
 const SimpleDialog = (props: SimpleDialogProps) => {
-  const { open, handleClose } = props
-  const accrodionRef = useRef<HTMLDivElement>(null)
-  const cardsRef = useRef<HTMLDivElement>(null)
-  const [openAccordion, setOpenAccordion] = useState(false)
+    const { open, handleClose } = props
+    const accrodionRef = useRef<HTMLDivElement>(null)
+    const cardsRef = useRef<HTMLDivElement>(null)
+    const [openAccordion, setOpenAccordion] = useState(false)
 
-  const handleOpenAccrordion = () => {
-    if(!openAccordion) {
-      accrodionRef.current?.scrollIntoView({ behavior: 'smooth' }) 
-    } else {
-      cardsRef.current?.scrollIntoView({ behavior: 'smooth'})
+    const handleOpenAccrordion = () => {
+        if (!openAccordion) {
+            accrodionRef.current?.scrollIntoView({ behavior: 'smooth' })
+        } else {
+            cardsRef.current?.scrollIntoView({ behavior: 'smooth' })
+        }
+
+        setOpenAccordion(!openAccordion)
     }
 
-    setOpenAccordion(!openAccordion)
-  }
-
-  return (
-    <Dialog
-      onClose={handleClose}
-      open={open}
-      maxWidth={'xl'}
-      className="dialog"
-    >
-      <div className='before-simple-dialog'>
-      <div className="SimpleDialog" ref={cardsRef}>
-        <UserCard isDialog={true}/>
-        <Card
-          sx={{
-            margin: '185px 0 0 20px',
-            padding: '15px 30px 15px 30px',
-            height: '335px',
-          }}
+    return (
+        <Dialog
+            onClose={handleClose}
+            open={open}
+            maxWidth={'xl'}
+            className="dialog"
         >
-          <div className="simple-dialog-container">
-            <div className="simple-dialog-container-row">
-              <span className="simple-dialog-container-row-key">About me:</span>
-              <span className="simple-dialog-container-row-value">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad odio
-                nemo rerum similique soluta. Asperiores voluptates quas
-                consequuntur nam repellendus cumque ipsam iusto tempora quae
-                veniam magni excepturi, esse aliquam!
-              </span>
-            </div>
-            <div className="simple-dialog-container-row">
-              <span className="simple-dialog-container-row-key">
-                Experience:
-              </span>
-              <span className="simple-dialog-container-row-value">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad hic
-                itaque veniam animi. Amet, in illo suscipit repellat fuga veniam
-                incidunt cum nam iure facere? Ipsum voluptas reprehenderit omnis
-                veritatis.
-              </span>
-            </div>
-            <div className="simple-dialog-container-row">
-              <span className="simple-dialog-container-row-key">
-                Why should you hire me:
-              </span>
-              <span className="simple-dialog-container-row-value">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. In
-                mollitia accusantium eos ducimus veniam. Deleniti tenetur
-                adipisci tempora? Porro, tempore! Necessitatibus, voluptates
-                dolore nostrum adipisci optio temporibus autem illo inventore?
-              </span>
-            </div>
-          </div>
-        </Card>
-      </div>
-      <div className="accordion">
-        <Accordion sx={{ width: '880px' }} ref={accrodionRef}>
-          <AccordionSummary>
-            <div className="accordion-summary">
-              <Button variant="contained" color="primary" onClick={handleOpenAccrordion} sx={{fontWeight: 700}}>
-                I want to hire you
-                <ExpandMoreIcon sx={{
-                  transition: '0.2s ease-in-out', 
-                  transform: (openAccordion) ? 'rotate(180deg)' : ''
-                  }}/>
-              </Button>
-            </div>
-          </AccordionSummary>
-          <AccordionDetails>
-            <div className="accordion-form">
-              <div className='accordion-form-description'>
-                <div className='accordion-form-description-value'>
-                  <span>
-                  Here you write a{' '}
-                    <span className='accordion-form-description-value-bold'>
-                    description
-                    </span>
-                  {' '}of your problem. In it, you should try to explain 
-                  what you need as precise as possible. Remember - the more information 
-                  about the problem you provide, the better chances you your problem
-                  will be fixed!
-                  </span>
+            <div className="before-simple-dialog">
+                <div className="SimpleDialog" ref={cardsRef}>
+                    <UserCard isDialog={true} />
+                    <Card
+                        sx={{
+                            margin: '185px 0 0 20px',
+                            padding: '15px 30px 15px 30px',
+                            height: '335px',
+                        }}
+                    >
+                        <div className="simple-dialog-container">
+                            <div className="simple-dialog-container-row">
+                                <span className="simple-dialog-container-row-key">
+                                    About me:
+                                </span>
+                                <span className="simple-dialog-container-row-value">
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Ad odio nemo rerum
+                                    similique soluta. Asperiores voluptates quas
+                                    consequuntur nam repellendus cumque ipsam
+                                    iusto tempora quae veniam magni excepturi,
+                                    esse aliquam!
+                                </span>
+                            </div>
+                            <div className="simple-dialog-container-row">
+                                <span className="simple-dialog-container-row-key">
+                                    Experience:
+                                </span>
+                                <span className="simple-dialog-container-row-value">
+                                    Lorem ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Ad hic itaque veniam
+                                    animi. Amet, in illo suscipit repellat fuga
+                                    veniam incidunt cum nam iure facere? Ipsum
+                                    voluptas reprehenderit omnis veritatis.
+                                </span>
+                            </div>
+                            <div className="simple-dialog-container-row">
+                                <span className="simple-dialog-container-row-key">
+                                    Why should you hire me:
+                                </span>
+                                <span className="simple-dialog-container-row-value">
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. In mollitia accusantium
+                                    eos ducimus veniam. Deleniti tenetur
+                                    adipisci tempora? Porro, tempore!
+                                    Necessitatibus, voluptates dolore nostrum
+                                    adipisci optio temporibus autem illo
+                                    inventore?
+                                </span>
+                            </div>
+                        </div>
+                    </Card>
                 </div>
-              </div>
-              <div className="accordion-form-details">
-                <TextField
-                  label="Description of your problem"
-                  variant="outlined"
-                  multiline
-                  minRows={6}
-                />
-                <div>
-                  <input type="checkbox" />
-                  <span>
-                    I have read and agreed to the Terms and Conditions
-                    <span>
-                      *
-                    </span>
-                  </span>
+                <div className="accordion">
+                    <Accordion sx={{ width: '880px' }} ref={accrodionRef}>
+                        <AccordionSummary>
+                            <div className="accordion-summary">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={handleOpenAccrordion}
+                                    sx={{ fontWeight: 700 }}
+                                >
+                                    I want to hire you
+                                    <ExpandMoreIcon
+                                        sx={{
+                                            transition: '0.2s ease-in-out',
+                                            transform: openAccordion
+                                                ? 'rotate(180deg)'
+                                                : '',
+                                        }}
+                                    />
+                                </Button>
+                            </div>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <div className="accordion-form">
+                                <div className="accordion-form-description">
+                                    <div className="accordion-form-description-value">
+                                        <span>
+                                            Here you write a{' '}
+                                            <span className="accordion-form-description-value-bold">
+                                                description
+                                            </span>{' '}
+                                            of your problem. In it, you should
+                                            try to explain what you need as
+                                            precise as possible. Remember - the
+                                            more information about the problem
+                                            you provide, the better chances you
+                                            your problem will be fixed!
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="accordion-form-details">
+                                    <TextField
+                                        label="Description of your problem"
+                                        variant="outlined"
+                                        multiline
+                                        minRows={6}
+                                    />
+                                    <div>
+                                        <input type="checkbox" />
+                                        <span>
+                                            I have read and agreed to the Terms
+                                            and Conditions
+                                            <span>*</span>
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <Button
+                                            variant="contained"
+                                            sx={{
+                                                fontWeight: 700,
+                                                width: '100%',
+                                            }}
+                                        >
+                                            <span className="accordion-form-details-button">
+                                                Send{' '}
+                                                <SendIcon
+                                                    fontSize="small"
+                                                    sx={{
+                                                        padding: '0 0 0 5px',
+                                                    }}
+                                                />
+                                            </span>
+                                        </Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </AccordionDetails>
+                    </Accordion>
                 </div>
-                <div>
-                  <Button variant='contained' sx={{fontWeight: 700, width: '100%'}}>
-                    <span className='accordion-form-details-button'>
-                      Send <SendIcon fontSize='small' sx={{padding: '0 0 0 5px'}}/>
-                    </span>
-                  </Button>
-                </div>
-              </div>
             </div>
-          </AccordionDetails>
-        </Accordion>
-      </div>
-      </div> 
-    </Dialog>
-  )
+        </Dialog>
+    )
 }
 
 export default SimpleDialog
