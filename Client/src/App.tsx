192f49b9432f6ad18c5aeb05139a2b48d28657bc
Maciej Słupianek <maciej.slupianekge@gmail.com>
2023-11-07 20:59:51 +0100

    User-card-final

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,7 +2,6 @@ import './Style/scss/App.scss'
 import HomePage from './Pages/HomePage'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
-import UserCard from './Components/UserCard'
 
 function App() {
   return (
@@ -10,11 +9,6 @@ function App() {
       <Header />
       <HomePage />
       <Footer />
-      <div style={{ display: 'flex', gap: '50px' }}>
-        <UserCard />
-        <UserCard />
-        <UserCard />
-      </div>
     </div>
   )
 }
