a3ef626524c6654c6b967eead8bd5f35babab0d8
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2023-11-17 11:20:16 +0100

    refactor exceptions

--- UserService/src/main/java/it/homeo/userservice/exceptions/AppUserNotFoundException.java
+++ UserService/src/main/java/it/homeo/userservice/exceptions/AppUserNotFoundException.java
@@ -1,11 +1,7 @@
 package it.homeo.userservice.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class AppUserNotFoundException extends RuntimeException {
     public AppUserNotFoundException(String id) {
-        super("User with id '%s' not found".formatted(id));
+        super("User with id '%s' not found.".formatted(id));
     }
 }
--- UserService/src/main/java/it/homeo/userservice/exceptions/ForbiddenException.java
+++ UserService/src/main/java/it/homeo/userservice/exceptions/ForbiddenException.java
@@ -1,11 +1,7 @@
 package it.homeo.userservice.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
 public class ForbiddenException extends RuntimeException {
     public ForbiddenException() {
-        super("Forbidden");
+        super("Access denied: You do not have the necessary permissions to perform this operation.");
     }
 }
