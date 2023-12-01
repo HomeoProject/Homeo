33c42d1a3ca53cc6cfe5c147d5ee2b3bd27ba265
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2023-12-01 13:53:45 +0100

    refactor

--- UserService/src/main/java/it/homeo/userservice/dtos/CheckAppUserAfterLoginRequest.java
+++ UserService/src/main/java/it/homeo/userservice/dtos/CheckAppUserAfterLoginRequest.java
@@ -3,7 +3,9 @@ package it.homeo.userservice.dtos;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 public record CheckAppUserAfterLoginRequest(
         @NotBlank
         String id,
