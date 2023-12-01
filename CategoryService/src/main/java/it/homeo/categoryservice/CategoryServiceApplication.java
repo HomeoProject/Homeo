8f7bb80e09c82bcfb6c73e975222e19254298583
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2023-12-01 19:15:43 +0100

    init and readme

---
+++ CategoryService/src/main/java/it/homeo/categoryservice/CategoryServiceApplication.java
@@ -0,0 +1,13 @@
+package it.homeo.categoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryServiceApplication.class, args);
+	}
+
+}
