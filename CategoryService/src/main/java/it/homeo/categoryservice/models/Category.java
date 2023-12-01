4797fe457ecc61c8c347fee8bda1ef806c1d8301
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2023-12-01 19:59:30 +0100

    category model

---
+++ CategoryService/src/main/java/it/homeo/categoryservice/models/Category.java
@@ -0,0 +1,46 @@
+package it.homeo.categoryservice.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @SequenceGenerator(
+            name = "category_id_sequence",
+            sequenceName = "category_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "category_id_sequence"
+    )
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String createdBy;
+
+    private String updatedBy;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        updatedAt = createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
