3725e22f9a7040dac35e1eeaefb3f979a52d87da
Maciej Słupianek <maciej.slupianekge@gmail.com>
2024-03-03 18:55:12 +0100

    sort-limit-active-users

--- Client/src/Pages/AdvertsPage.tsx
+++ Client/src/Pages/AdvertsPage.tsx
@@ -3,6 +3,12 @@ import SearchIcon from '@mui/icons-material/Search'
 import UserCard from '../Components/UserCard'
 import FiltersDialog from '../Components/FiltersDialog'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import MenuItem from '@mui/material/MenuItem';
+import PersonIcon from '@mui/icons-material/Person';
+import Badge from '@mui/material/Badge';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Tooltip from '@mui/material/Tooltip';
+import Pagination from '@mui/material/Pagination';
 import '../style/scss/AdvertsPage.scss'
 
 const AdvertsPage = () => {
@@ -13,8 +19,11 @@ const AdvertsPage = () => {
     }
 
     const [screenSize, setScreenSize] = useState(getCurrentDimension())
-    const [open, setOpen] = useState<boolean>(false)
+    const [openSearch, setOpenSearch] = useState<boolean>(false)
     const [filterClicked, setFilterClicked] = useState<number>(0)
+    const [sortValue, setSortValue] = useState<string>('')
+    const [perPageValue, setPerPageValue] = useState<string>('12')
+    const [onlyActiveUsers,setOnlyActiveUsers] = useState<boolean>(true)
 
     const filters = [
         'Category',
@@ -26,14 +35,25 @@ const AdvertsPage = () => {
         'Location',
     ]
 
-    const handleClickOpen = (index: number) => {
+    const handleClickOpenSearch = (index: number) => {
         setFilterClicked(index)
-        console.log(index)
-        setOpen(true)
+        setOpenSearch(true)
     }
 
-    const handleClose = () => {
-        setOpen(false)
+    const handleCloseSearch = () => {
+        setOpenSearch(false)
+    }
+
+    const handleSortChange = (event: SelectChangeEvent) => {
+        setSortValue(event.target.value);
+      };
+
+    const handlePerPageChange = (event: SelectChangeEvent) => {
+        setPerPageValue(event.target.value);
+    }
+
+    const handleActiveUsersChange = () => {
+        setOnlyActiveUsers(!onlyActiveUsers)
     }
 
     useEffect(() => {
@@ -73,7 +93,7 @@ const AdvertsPage = () => {
                             {filters.map((filter, index) => {
                                 return (
                                     <div
-                                        onClick={() => handleClickOpen(index)}
+                                        onClick={() => handleClickOpenSearch(index)}
                                         className="adverts-page-search-filters-container"
                                     >
                                         <span className="adverts-page-search-filters-container-label">
@@ -99,6 +119,51 @@ const AdvertsPage = () => {
                     <span className="adverts-page-cards-label-value">
                         Adverts for {searchValue}
                     </span>
+                    <div className='adverts-page-cards-label-right'>
+                        <div>
+                            <Tooltip title={onlyActiveUsers ? 'active users only' : 'all users'}>
+                                <Badge color='primary' variant={onlyActiveUsers ? 'dot' : 'standard'} overlap="circular">
+                                    <PersonIcon fontSize='large' onClick={handleActiveUsersChange}/>
+                                </Badge>
+                            </Tooltip>
+                        </div>
+                        <div className='adverts-page-cards-label-page'>
+                            <span>
+                                Per page:
+                            </span>
+                            <Select
+                                value={perPageValue}
+                                onChange={handlePerPageChange}
+                                displayEmpty
+                                inputProps={{ 'aria-label': 'Without label' }}
+                            >
+                                <MenuItem value="">
+                                    None
+                                </MenuItem>
+                                <MenuItem value={'12'}>12</MenuItem>
+                                <MenuItem value={'24'}>24</MenuItem>
+                                <MenuItem value={'36'}>36</MenuItem>
+                            </Select>
+                        </div>
+                        <div className='adverts-page-cards-label-filters'>
+                            <span>
+                                Sort by:
+                            </span>
+                            <Select
+                                value={sortValue}
+                                onChange={handleSortChange}
+                                displayEmpty
+                                inputProps={{ 'aria-label': 'Without label' }}
+                            >
+                                <MenuItem value="">
+                                    None
+                                </MenuItem>
+                                <MenuItem value={'ascending'}>Price ascending</MenuItem>
+                                <MenuItem value={'descending'}>Price descending</MenuItem>
+                                <MenuItem value={'newest'}>Newest</MenuItem>
+                            </Select>
+                        </div>
+                    </div>   
                 </div>
                 <div className="adverts-page-cards-grid">
                     <UserCard isDialog={false} />
@@ -106,10 +171,13 @@ const AdvertsPage = () => {
                     <UserCard isDialog={false} />
                     <UserCard isDialog={false} />
                 </div>
+                <div className='adverts-page-cards-pagination'>
+                    <Pagination count={10} color="primary" />
+                </div>
             </div>
             <FiltersDialog
-                open={open}
-                handleClose={handleClose}
+                open={openSearch}
+                handleClose={handleCloseSearch}
                 openFilter={filterClicked}
             />
         </div>
