0c25d79b155f2be9a60978d696ff1604b11d08c1
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2024-03-05 22:28:03 +0100

    fix: fix repositories silly bugs

--- ConstructorService/src/main/java/it/homeo/constructorservice/repositories/ConstructorRepository.java
+++ ConstructorService/src/main/java/it/homeo/constructorservice/repositories/ConstructorRepository.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConstructorRepository extends JpaRepository<Long, Constructor> { }
+public interface ConstructorRepository extends JpaRepository<Constructor, Long> { }
