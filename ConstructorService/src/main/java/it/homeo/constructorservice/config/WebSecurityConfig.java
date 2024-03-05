4cf9e3f2fff783fe1bcd15d68d4123aa99b074fb
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2024-03-05 19:53:28 +0100

    feat: constructor-service inti with basic openapi config and websecurity

---
+++ ConstructorService/src/main/java/it/homeo/constructorservice/config/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package it.homeo.constructorservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /*
+        This is where we configure the security required for our endpoints and set up our app to serve as
+        an OAuth2 Resource Server, using JWT validation.
+        */
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .cors(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(makePermissionsConverter()))
+                )
+                .build();
+    }
+
+    private JwtAuthenticationConverter makePermissionsConverter() {
+        final var jwtAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtAuthoritiesConverter.setAuthoritiesClaimName("permissions");
+        jwtAuthoritiesConverter.setAuthorityPrefix("");
+
+        final var jwtAuthConverter = new JwtAuthenticationConverter();
+        jwtAuthConverter.setJwtGrantedAuthoritiesConverter(jwtAuthoritiesConverter);
+
+        return jwtAuthConverter;
+    }
+}
---
+++ ConstructorService/src/test/java/it/homeo/constructorservice/ConstructorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.homeo.constructorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
