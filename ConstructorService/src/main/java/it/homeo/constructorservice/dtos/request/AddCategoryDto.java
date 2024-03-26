c6ef12cf6d7182ee40dff959857d38643397fa43
Piotr Damrych <piotr.damrych2002@gmail.com>
2024-03-26 16:34:42 +0100

    feat: added better validation responses

--- ConstructorService/src/main/java/it/homeo/constructorservice/dtos/request/AddCategoryDto.java
+++ ConstructorService/src/main/java/it/homeo/constructorservice/dtos/request/AddCategoryDto.java
@@ -4,11 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record AddCategoryDto(
-        @NotBlank
-        @Size(max = 50)
+        @NotBlank(message = "Name cannot be empty")
+        @Size(max = 50, message = "Name cannot exceed 50 characters")
         String name,
 
-        @NotBlank
-        @Size(max = 255)
+        @NotBlank(message = "Description cannot be empty")
+        @Size(max = 255, message = "Description cannot exceed 255 characters")
         String description
 ) { }
--- ConstructorService/src/main/java/it/homeo/constructorservice/dtos/request/UpdateConstructorDto.java
+++ ConstructorService/src/main/java/it/homeo/constructorservice/dtos/request/UpdateConstructorDto.java
@@ -9,35 +9,35 @@ import jakarta.validation.constraints.*;
 import java.util.Set;
 
 public record UpdateConstructorDto(
-        @NotBlank
+        @NotBlank(message = "About me cannot be empty")
         String aboutMe,
 
-        @NotBlank
+        @NotBlank(message = "Experience cannot be empty")
         String experience,
 
-        @Min(1)
+        @Min(value = 1, message = "Minimum rate must be at least 1")
         Integer minRate,
 
-        @NotBlank
-        @Pattern(regexp = "^(?:\\+48)?\\d{9}$")
+        @NotBlank(message = "Phone number cannot be empty")
+        @Pattern(regexp = "^(?:\\+48)?\\d{9}$", message = "Invalid phone number format")
         String phoneNumber,
 
-        @NotBlank
-        @Email
+        @NotBlank(message = "Constructor email cannot be empty")
+        @Email(message = "Invalid email format")
         String constructorEmail,
 
         @ValidLanguages
-        @Size(min= 1, max = 6)
+        @Size(min = 1, max = 6, message = "Languages must be between 1 and 6")
         Set<String> languages,
 
         @ValidCities
-        @Size(min= 1, max = 6)
+        @Size(min = 1, max = 6, message = "Cities must be between 1 and 6")
         Set<String> cities,
 
-        @Size(min= 1, max = 10)
+        @Size(min = 1, max = 10, message = "Category IDs must be between 1 and 10")
         Set<Long> categoryIds,
 
         @ValidPaymentMethods
-        @NotEmpty
+        @NotEmpty(message = "Payment methods cannot be empty")
         Set<PaymentMethod> paymentMethods
 ) { }
