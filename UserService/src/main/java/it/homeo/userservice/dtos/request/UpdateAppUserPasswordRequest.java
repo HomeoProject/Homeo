f3f0092c4c3a5505abdbba56e7831146f83721e6
Piotr Damrych <piotr.damrych2002@gmail.com>
2024-03-26 16:25:04 +0100

    feat: added better validation responses

--- UserService/src/main/java/it/homeo/userservice/dtos/request/UpdateAppUserPasswordRequest.java
+++ UserService/src/main/java/it/homeo/userservice/dtos/request/UpdateAppUserPasswordRequest.java
@@ -3,4 +3,4 @@ package it.homeo.userservice.dtos.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record UpdateAppUserPasswordRequest(@NotBlank @NotNull String password) { }
+public record UpdateAppUserPasswordRequest(@NotBlank(message = "Password cannot be empty") String password) { }
