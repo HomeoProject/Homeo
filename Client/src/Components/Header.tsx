2d827584c1fc24760cb76835711c504bc041ee60
Maciej Słupianek <maciej.slupianekge@gmail.com>
2024-03-30 00:49:09 +0100

    run-prettier

--- Client/src/Components/Header.tsx
+++ Client/src/Components/Header.tsx
@@ -1,7 +1,7 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import '../Style/scss/Header.scss'
 import HeaderDrawer from './HeaderDrawer'
-import PublicIcon from '@mui/icons-material/Public';
+import PublicIcon from '@mui/icons-material/Public'
 import { Link } from 'react-router-dom'
 import AccountMenu from './AccountMenu'
 import { useAuth0 } from '@auth0/auth0-react'
@@ -9,25 +9,27 @@ import { useDictionaryContext } from '../Context/DictionaryContext'
 import LoginButton from './LoginButton'
 import homeIcon from '../Assets/icon-cut.png'
 import AdminPanelLink from './AdminPanelLink'
-import Tooltip from '@mui/material/Tooltip';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import { english, polish } from '../Data/dictionary';
+import Tooltip from '@mui/material/Tooltip'
+import IconButton from '@mui/material/IconButton'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import { english, polish } from '../Data/dictionary'
 
 const Header = () => {
   const { isAuthenticated } = useAuth0()
   const { dictionary, setDictionary } = useDictionaryContext()
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
   const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(e.currentTarget);
-  };
+    setAnchorEl(e.currentTarget)
+  }
   const handleClose = (e: React.MouseEvent<HTMLElement>) => {
     console.log(e.currentTarget.dataset.myValue)
-    setDictionary(e.currentTarget.dataset.myValue === 'polish' ? polish : english);
-    setAnchorEl(null);
-  };
+    setDictionary(
+      e.currentTarget.dataset.myValue === 'polish' ? polish : english
+    )
+    setAnchorEl(null)
+  }
 
   return (
     <div className="Header">
@@ -50,17 +52,14 @@ const Header = () => {
             {!isAuthenticated ? <LoginButton /> : <AccountMenu />}
           </div>
           <Tooltip title="Language">
-            <IconButton 
+            <IconButton
               onClick={handleClick}
               size="small"
               aria-controls={open ? 'account-menu' : undefined}
               aria-haspopup="true"
               aria-expanded={open ? 'true' : undefined}
-              >
-              <PublicIcon 
-                fontSize='large' 
-                sx={{color: '#fafafa'}}
-              />
+            >
+              <PublicIcon fontSize="large" sx={{ color: '#fafafa' }} />
             </IconButton>
           </Tooltip>
           <Menu
@@ -98,19 +97,13 @@ const Header = () => {
             transformOrigin={{ horizontal: 'right', vertical: 'top' }}
             anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
           >
-        <MenuItem 
-          onClick={handleClose}
-          data-my-value="polish"
-          >
-           Polish
-        </MenuItem>
-        <MenuItem 
-          onClick={handleClose}
-          data-my-value="english"
-          >
-           English
-        </MenuItem>
-      </Menu>
+            <MenuItem onClick={handleClose} data-my-value="polish">
+              Polish
+            </MenuItem>
+            <MenuItem onClick={handleClose} data-my-value="english">
+              English
+            </MenuItem>
+          </Menu>
         </div>
         <div className="header-nav-small">
           <HeaderDrawer />
