c5eca73d5c3648f97d20cb5e2e8a3abe0cc664d9
Piotr Damrych <piotr.damrych2002@gmail.com>
2024-04-06 15:51:32 +0200

    feat: rabbitmq and async config

---
+++ SearchService/src/main/java/it/homeo/searchservice/config/RabbitMQConfig.java
@@ -0,0 +1,46 @@
+package it.homeo.searchservice.config;
+
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.interceptor.RetryOperationsInterceptor;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+
+@Configuration
+public class RabbitMQConfig {
+
+    private final CachingConnectionFactory cachingConnectionFactory;
+
+    public RabbitMQConfig(CachingConnectionFactory cachingConnectionFactory) {
+        this.cachingConnectionFactory = cachingConnectionFactory;
+    }
+
+    @Bean
+    public RetryOperationsInterceptor retryInterceptor() {
+        return RetryInterceptorBuilder.stateless().maxAttempts(3)
+                .backOffOptions(2000, 2.0, 100000)
+                .recoverer(new RejectAndDontRequeueRecoverer())
+                .build();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(SimpleRabbitListenerContainerFactoryConfigurer configurer) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, cachingConnectionFactory);
+        factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
+        factory.setAdviceChain(retryInterceptor());
+        factory.setDefaultRequeueRejected(false);
+        return factory;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
