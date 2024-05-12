b267640bd54aec68737d5d55c26a04d883cde145
Piotr Damrych <piotr.damrych2002@gmail.com>
2024-05-12 15:18:27 +0200

    fix: fixed client dockerfile, api ninjas url

--- Client/src/AxiosClients/apiNinjasClient.ts
+++ Client/src/AxiosClients/apiNinjasClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiNinjasClient = axios.create({
-  baseURL: 'https://api.api-ninjas.com/v1/',
+  baseURL: import.meta.env.VITE_API_NINJAS_URL,
   headers: {
     'X-Api-Key': import.meta.env.VITE_API_NINJAS_KEY as string,
     'Content-Type': 'application/json',
