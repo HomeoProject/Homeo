109bfcc7c3a0b949fe86c102359793239a11fbeb
Karol-Wisniewski <karol-wisniewski2001@wp.pl>
2024-07-01 17:03:39 +0200

    chore: update api ninjas client config

--- Client/src/AxiosClients/apiNinjasClient.ts
+++ Client/src/AxiosClients/apiNinjasClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiNinjasClient = axios.create({
-  baseURL: `${import.meta.env.VITE_API_NINJAS_URL}/v1`,
+  baseURL: `${window.location.protocol}//${window.location.hostname}/v1`,
   headers: {
     'X-Api-Key': import.meta.env.VITE_API_NINJAS_KEY as string,
     'Content-Type': 'application/json',
